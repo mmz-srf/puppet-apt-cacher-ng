7f3830630781292a0d5c75842c2b343f487f7825
Bruno Mazenauer <bruno.mazenauer@srf.ch>
2018-01-10 10:51:01 +0100

    - removed configuration file. the version installed by package is now used. - added BindAddress and DontCache directive

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,8 @@
-class apt-cacher-ng($version = 'installed') {
+class apt-cacher-ng(
+  $version = 'installed',
+  $bind_address = false,   # BindAddress
+  $dont_cache   = false,   # DontCache
+) {
   package { 'apt-cacher-ng':
     ensure => $version,
   }
@@ -8,17 +12,26 @@ class apt-cacher-ng($version = 'installed') {
     require => Package['apt-cacher-ng'],
   }
 
-  file { '/etc/apt-cacher-ng/acng.conf':
-    source => ["puppet:///modules/site-apt-cacher-ng/${::fqdn}/acng.conf",
-               'puppet:///modules/site-apt-cacher-ng/acng.conf',
-               'puppet:///modules/apt-cacher-ng/acng.conf'],
-    notify  => Service['apt-cacher-ng'],
-    require => Package['apt-cacher-ng'],
-  }
-
   file { '/var/cache/apt-cacher-ng':
     ensure  => directory,
     owner   => 'apt-cacher-ng',
     require => Package['apt-cacher-ng'],
   }
+
+  file { '/etc/apt-cacher-ng/bind_address.conf':
+    ensure  => $bind_address ? { false => absent, default => present },
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => inline_template("BindAddress: <%= @bind_address %>"),
+  }
+
+  file { '/etc/apt-cacher-ng/dont_cache.conf':
+    ensure  => $dont_cache ? { false => absent, default => present },
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => inline_template("DontCache: <%= if (@dont_cache) then @dont_cache.join(' ') end %>"),
+  }
+
 }
